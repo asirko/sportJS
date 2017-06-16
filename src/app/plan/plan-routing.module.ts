666228036c348657aee2afa541e3464667cf884b
asirko <sirko.alexandre@gmail.com>
2017-06-16 22:25:35 +0200

    Added Home and complete header

--- src/app/plan/plan-routing.module.ts
+++ src/app/plan/plan-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProgramsComponent } from './programs/programs.component';
+import { ProgramComponent } from './program/program.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ProgramsComponent
+}, {
+  path: 'detail',
+  component: ProgramComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/root/root-routing.module.ts
+++ src/app/root/root-routing.module.ts
@@ -5,6 +5,10 @@ import { UnknownComponent } from './unknown/unknown.component';
 
 const routes: Routes = [{
   path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}, {
+  path: 'home',
   component: HomeComponent
 }, {
   path: 'plan',
