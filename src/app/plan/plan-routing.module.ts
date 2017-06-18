73e298607b0beb9833c8c635d466d53dcc41b485
asirko <sirko.alexandre@gmail.com>
2017-06-18 19:44:29 +0200

    added first form style + first input

--- src/app/plan/plan-routing.module.ts
+++ src/app/plan/plan-routing.module.ts
@@ -4,12 +4,13 @@ import { ProgramsComponent } from './programs/programs.component';
 import { ProgramComponent } from './program/program.component';
 
 const routes: Routes = [{
-  path: '',
-  component: ProgramsComponent
-}, {
-  path: 'detail',
-  component: ProgramComponent
-}];
+    path: '',
+    component: ProgramsComponent
+  }, {
+    path: 'detail',
+    component: ProgramComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
