b52bb43ec9d512863f164d0582225735b349b41c
asirko <jrbukx51Alex2628>
2017-07-13 18:01:46 +0200

    initiat the exercice store to share selected exercice

---
+++ src/app/stat/exercice-store.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ExerciceStoreDirective } from './exercice-store.directive';
+
+describe('ExerciceStoreDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ExerciceStoreDirective();
+    expect(directive).toBeTruthy();
+  });
+});
