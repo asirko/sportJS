adc91e01f43cd72708b6ccdbeabd36227b8bd28e
asirko <sirko.alexandre@gmail.com>
2017-07-15 21:27:16 +0200

    exo on http

--- mock/api/auth/auth.middleware.js
+++ mock/api/auth/auth.middleware.js
@@ -3,14 +3,16 @@ const tokens = require('../auth/auth.token');
 
 exports.requireLogin = function (req, res, next) {
 
-  const authorization = req.header('Authorization');
-  let token;
-  if (authorization) {
-    token = /^Bearer (.*)/.exec(authorization)[1];
-  }
-  if (token === tokens.test) {
-    next();
-  } else {
-    res.sendStatus(401);
-  }
+  next();
+
+  // const authorization = req.header('Authorization');
+  // let token;
+  // if (authorization) {
+  //   token = /^Bearer (.*)/.exec(authorization)[1];
+  // }
+  // if (token === tokens.test) {
+  //   next();
+  // } else {
+  //   res.sendStatus(401);
+  // }
 };
--- src/app/shared/program/program.service.ts
+++ src/app/shared/program/program.service.ts
@@ -1,44 +1,56 @@
 import { Injectable } from '@angular/core';
-
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/do';
-
 import { Program } from './program';
-import { HttpAuthService } from '../../security/http-auth.service';
 
 @Injectable()
 export class ProgramService {
 
-  constructor(private http: HttpAuthService) { }
+  constructor() { }
 
+  /**
+   * voir la page /plan, il doit y avoir 3 éléments
+   * @returns {null}
+   */
   findAll(): Observable<Program[]> {
-    return this.http.get(`/api/programs`)
-      .map(res => res.json());
+    return null;
   }
 
+  /**
+   * voir la page /run, le switch doit avoir 3 élément + autre
+   * @returns {null}
+   */
   findAllPlusEmpty(): Observable<Program[]> {
-    return this.findAll()
-      .map(list => [...list, {name: 'Autre'}]);
+    return null;
   }
 
-  updateProgram(index: number, program: Program): void {
-    this.http.post(`/api/programs/${index}`, program)
-      .subscribe(() => {});
-  }
-
-  addProgram(program: Program): void {
-    this.http.post(`/api/programs`, program)
-      .subscribe(() => {});
-  }
-
-  deleteProgram(index: number): void {
-    this.http.delete(`/api/programs/${index}`)
-      .subscribe(() => {});
-  }
-
+  /**
+   * accéder au détail d'un programme /plan/0
+   * @param index
+   * @returns {null}
+   */
   getProgramByIndex(index: number): Observable<any> {
-    return this.http.get(`/api/programs/${index}`)
-      .map(res => res.json());
+    return null;
+  }
+
+  /**
+   * faites une mise à jour d'un programme (description ?) et faites un rafraïchissment de la page
+   * @param index
+   * @param program
+   */
+  updateProgram(index: number, program: Program): void {
+  }
+
+  /**
+   * créez un programme
+   * @param program
+   */
+  addProgram(program: Program): void {
+  }
+
+  /**
+   * supprimer un programme
+   * @param index
+   */
+  deleteProgram(index: number): void {
   }
 }
