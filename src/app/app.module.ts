c7af283d2221ebc8dd1cb78e49bee9cf10864869
asirko <alexandre@jump-biz.com>
2018-04-16 18:43:42 +0200

    update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RootModule } from './root/root.module';
 import { SecurityModule } from './security/security.module';
+
+import localeFr from '@angular/common/locales/fr';
+import localeFrExtra from '@angular/common/locales/extra/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeFr, 'fr-FR', localeFrExtra);
 
 @NgModule({
   declarations: [AppComponent],
@@ -10,7 +16,7 @@ import { SecurityModule } from './security/security.module';
     RootModule,
     SecurityModule
   ],
-  providers: [{ provide: LOCALE_ID, useValue: "fr-FR" }],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
